16179da22a607269137d765d13da766d217f5558
stragger <skrebkov.art@gmail.com>
2014-09-29 22:26:23 +0400

    Initial commit:add photoeffect module

---
+++ modules/photoeffects/include/photoeffects.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+int sepia(cv::InputArray src, cv::OutputArray dst);
+
+int filmGrain(cv::InputArray src, cv::OutputArray dst, int grainValue = 8, cv::RNG& rng=cv::theRNG());
+
+int fadeColor(cv::InputArray src, cv::OutputArray dst,cv::Point startPoint,cv::Point endPoint);
+
+int tint(cv::InputArray src, cv::OutputArray dst, const cv::Vec3b &colorTint, float density);
+
+int glow(cv::InputArray src, cv::OutputArray dst, int radius = 0, float intensity = 0.0f);
+
+int edgeBlur(cv::InputArray src, cv::OutputArray dst, int indentTop, int indentLeft);
+
+int boostColor(cv::InputArray src, cv::OutputArray dst, float intensity = 0.0f);
+
+int antique(cv::InputArray src, cv::OutputArray dst, cv::InputArray texture, float alpha);
+
+int vignette(cv::InputArray src, cv::OutputArray dst, cv::Size rect);
+
+int warmify(cv::InputArray src, cv::OutputArray dst, uchar delta = 30);
+
+int matte(cv::InputArray src, cv::OutputArray dst, cv::Point firstPoint, cv::Point secondPoint,
+          float sigmaX, float sigmaY, cv::Size ksize=cv::Size(0, 0));
---
+++ modules/photoeffects/test/sepia_test.cpp
@@ -0,0 +1,54 @@
+#include "photoeffects.hpp"
+#include "test_utils.hpp"
+#include <gtest/gtest.h>
+
+using namespace cv;
+
+
+TEST(photoeffects, SepiaInvalidImageFormat)
+{
+    Mat src(10, 10, CV_8UC3);
+    Mat dst;
+
+    EXPECT_ERROR(CV_StsAssert, sepia(src, dst));
+}
+
+
+TEST(photoeffects, SepiaTest)
+{
+    Mat src(10, 10, CV_8UC1, Scalar(0)), dst, hsvDst;
+    vector<Mat> channels(3);
+
+    EXPECT_EQ(0, sepia(src, dst));
+    cvtColor(dst, hsvDst, CV_BGR2HSV);
+    split(hsvDst, channels);
+    EXPECT_LE(19 - 1, channels[0].at<uchar>(0, 0)); // hue = 19
+    EXPECT_GE(19 + 1, channels[0].at<uchar>(0, 0));
+    EXPECT_LE(78 - 1, channels[1].at<uchar>(0, 0)); // saturation = 78
+    EXPECT_GE(78 + 1, channels[1].at<uchar>(0, 0));
+    EXPECT_LE(src.at<uchar>(0, 0) + 20 - 1, channels[2].at<uchar>(0, 0));
+    EXPECT_GE(src.at<uchar>(0, 0) + 20 + 1, channels[2].at<uchar>(0, 0));
+}
+
+TEST(photoeffects, SepiaRegressionTest)
+{
+    string input = "./testdata/sepia_test.png";
+    string expectedOutput = "./testdata/sepia_test_result.png";
+
+    Mat image, rightDst;
+
+    image = imread(input, CV_LOAD_IMAGE_GRAYSCALE);
+    rightDst = imread(expectedOutput, CV_LOAD_IMAGE_COLOR);
+
+    if (image.empty())
+        FAIL() << "Can't read " + input + " image";
+    if (rightDst.empty())
+        FAIL() << "Can't read " + expectedOutput + " image";
+
+    Mat dst;
+    EXPECT_EQ(0, sepia(image, dst));
+
+    Mat diff = abs(rightDst - dst);
+    Mat mask = diff.reshape(1) > 1;
+    EXPECT_EQ(0, countNonZero(mask));
+}
