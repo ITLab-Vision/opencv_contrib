836fdce4515a140088c469ea9478009907677c9d
stragger <skrebkov.art@gmail.com>
2014-10-04 19:23:20 +0400

    Added parallel overlay

--- modules/photoeffects/src/glow.cpp
+++ modules/photoeffects/src/glow.cpp
@@ -1,44 +1,64 @@
 #include "precomp.hpp"
 #include <opencv2/core/utility.hpp>
 
+#include <stdio.h>
+#define TIMER_START(name) int64 t_##name = getTickCount()
+#define TIMER_END(name) printf("TIMER_" #name ":\t%6.2fms\n", \
+            1000.f * ((getTickCount() - t_##name) / getTickFrequency()))
+
 namespace cv { namespace photoeffects {
 
 namespace
 {
     const int COUNT_CHANNEL = 3;
-    void overlay(InputArray foreground, InputArray background, OutputArray result)
+
+    class OverlayInvoker: public cv::ParallelLoopBody
     {
-        Mat foreImg = foreground.getMat();
-        Mat backImg = background.getMat();
-
-        result.create(backImg.size(), backImg.type());
-        Mat resultImg = result.getMat();
-
-        int width = foreImg.cols;
-        int height = foreImg.rows;
-        for(int i = 0; i < height; i++)
+    public:
+        OverlayInvoker(const Mat &foreground, const Mat &background, Mat &result)
+            : foreImg(foreground),
+              backImg(background),
+              resImg(result),
+              width(foreground.cols)
         {
-            for(int j = 0; j < width; j++)
-            {
-                for(int k = 0; k < COUNT_CHANNEL; k++)
-                {
-                    uchar intensFore = foreImg.at<Vec3b>(i, j)[k];
-                    uchar intensBack = backImg.at<Vec3b>(i, j)[k];
-                    int intensResult = (2 * intensFore * intensBack) / 255;
+            resImg.create(foreImg.size(), foreImg.type());
+        }
 
-                    if (intensBack > 127)
+        void operator()(const Range& rows) const
+        {
+            Mat foreStripe = foreImg.rowRange(rows.start, rows.end);
+            Mat backStripe = backImg.rowRange(rows.start, rows.end);
+            Mat resStripe = resImg.rowRange(rows.start, rows.end);
+            int stripeHeight = foreStripe.rows;
+            for (int i = 0; i < stripeHeight; i++)
+            {
+                uchar* foreRow = foreStripe.row(i).data;
+                uchar* backRow = backStripe.row(i).data;
+                uchar* resRow = resStripe.row(i).data;
+                for (int j  = 0; j < width * COUNT_CHANNEL; j++)
+                {
+                    int intensResult = (2 * foreRow[j] * backRow[j]) / 255;
+
+                    if (backRow[j] > 127)
                     {
-                        intensResult = cv::min(-intensResult - 255 + 2 * (intensFore + intensBack), 255);
+                        intensResult = cv::min(-intensResult - 255 + 2 * (foreRow[j] + backRow[j]), 255);
                     }
-                    resultImg.at<Vec3b>(i, j)[k] = intensResult;
+                    resRow[j] = intensResult;
                 }
             }
         }
-    }
+    private:
+        const Mat &foreImg;
+        const Mat &backImg;
+        Mat &resImg;
+        int width;
+        //OverlayInvoker& operator=(const OverlayInvoker&);
+    };
 }
 
 int glow(InputArray src, OutputArray dst, int radius, float intensity)
 {
+    TIMER_START(all);
     Mat srcImg = src.getMat();
 
     CV_Assert(srcImg.channels() == COUNT_CHANNEL);
@@ -53,17 +73,20 @@ int glow(InputArray src, OutputArray dst, int radius, float intensity)
 
     Mat blurImg;
     Size size(radius, radius);
-
+    TIMER_START(boxFilt);
     boxFilter(srcImg, blurImg, -1, size);
-
+    TIMER_END(boxFilt);
+    TIMER_START(overl);
     Mat overlayImg;
-    overlay(blurImg, srcImg, overlayImg);
-
+    parallel_for_(Range(0, blurImg.rows), OverlayInvoker(blurImg, srcImg, overlayImg));
+    TIMER_END(overl);
+    TIMER_START(opac);
     uchar coeff = static_cast<uchar>(intensity * 255.0);
     Mat dstImg = (coeff * overlayImg + (255 - coeff) * srcImg) / 255;
-
+    TIMER_END(opac);
     dstImg.convertTo(dst, srcImgType);
 
+    TIMER_END(all);
     return 0;
 }
 
