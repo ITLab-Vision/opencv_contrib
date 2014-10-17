53acf82a56449b4113066e204143693d4556a977
Vlad <vladt9@yandex.ru>
2014-10-17 19:02:23 +0400

    Fixed trackbars of vignette sample

--- modules/photoeffects/samples/vignette_sample.cpp
+++ modules/photoeffects/samples/vignette_sample.cpp
@@ -18,7 +18,8 @@ Mat image, vignetteImg;
 Size rectSlider;
 
 int processArguments(int argc, char **argv, Mat &img);
-void on_trackbar(int, void*);
+void on_trackbar_width(int, void*);
+void on_trackbar_height(int, void*);
 
 int main(int argc, char** argv)
 {
@@ -31,14 +32,16 @@ int main(int argc, char** argv)
     namedWindow(srcImgWinName, WINDOW_FREERATIO);
     namedWindow(dstImgWinName, WINDOW_FREERATIO);
 
-    rectSlider.height = 0;
-    rectSlider.width = 0;
+    rectSlider.width = image.cols / 2.0f;
+    rectSlider.height = image.rows / 1.5f;
 
-    createTrackbar("Ellipse Width", srcImgWinName, &rectSlider.width, image.cols - 1, on_trackbar);
-    createTrackbar("Ellipse Height", srcImgWinName, &rectSlider.height, image.rows - 1, on_trackbar);
+    createTrackbar("Ellipse Width", srcImgWinName, &rectSlider.width, image.cols - 1, on_trackbar_width);
+    createTrackbar("Ellipse Height", srcImgWinName, &rectSlider.height, image.rows - 1, on_trackbar_height);
+
+    on_trackbar_width(rectSlider.width, 0);
+    on_trackbar_height(rectSlider.height, 0);
 
     imshow(srcImgWinName, image);
-    imshow(dstImgWinName, image);
 
     waitKey();
     destroyAllWindows();
@@ -55,12 +58,18 @@ int processArguments(int argc, char **argv, Mat &img)
     return 0;
 }
 
-void on_trackbar(int, void*)
+void on_trackbar_width(int, void*)
 {
     rectSlider.width++;
-    rectSlider.height++;
 
     vignette(image, vignetteImg, rectSlider);
     imshow(dstImgWinName, vignetteImg);
 }
 
+void on_trackbar_height(int, void*)
+{
+    rectSlider.height++;
+
+    vignette(image, vignetteImg, rectSlider);
+    imshow(dstImgWinName, vignetteImg);
+}
